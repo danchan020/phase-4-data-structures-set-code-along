404d80aedae71ba834d478326744cee186f043f8
Daniel <danchan020@gmail.com>
2022-08-18 11:15:42 -0700

    commit tree 8a37a0adead3bf2e19199c0f88191fa59cab7352

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,46 @@
-# your code here
+class MySet
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+          @hash[value] = true
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size
+        @hash.size
+    end
+
+    def clear
+        @hash.clear
+        self
+    end
+
+    def each(enumerable = [])
+        enumerable.each do |value|
+          self[value] = true
+        end
+        puts self
+    end
+
+    def inspect
+        @hash.inspect
+        print self
+    end
+end
+
+set = MySet.new([1, 2, 3, 3])
+
